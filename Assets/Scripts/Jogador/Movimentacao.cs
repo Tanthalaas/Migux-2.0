043e3cfd562b10cdd80d0d1476775ed99c20a04f
Aerton <aertoncastro@gmail.com>
2021-05-10 15:24:12 -0300

    :sparkles: Personagem olha pro mouse

--- Assets/Scripts/Jogador/Movimentacao.cs
+++ Assets/Scripts/Jogador/Movimentacao.cs
@@ -5,6 +5,7 @@ using DG.Tweening;
 
 public class Movimentacao : MonoBehaviour
 {
+    [SerializeField] Personagem personagem;
     [SerializeField] float velocidade;
     [SerializeField] float velocidadeDeSkate;
     bool andando;
@@ -26,6 +27,7 @@ public class Movimentacao : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0))
         {
+            OlharProMouse(true);
             andando = true;
             Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             transform.DOKill();
@@ -36,7 +38,9 @@ public class Movimentacao : MonoBehaviour
                     .SetUpdate(true)
                     .SetEase(Ease.Linear)
                     .OnComplete(() => andando = false);
-            } else {
+            } 
+            else 
+            {
                 transform
                     .DOMove(position, Vector2.Distance(position, transform.position) / velocidadeDeSkate)
                     .SetUpdate(true)
@@ -51,6 +55,42 @@ public class Movimentacao : MonoBehaviour
         if(!andando || forcar)
         {
             //TODO: Lógica de olhar para o mouse
+            Vector2 direcaoDoMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            float angulo = Vector2.SignedAngle(Vector2.right, direcaoDoMouse);
+            Debug.Log(angulo);
+
+            if(angulo >= 135f)
+            {
+                personagem.MostrarLado(PersonagemBase.Direcao.Esquerda);
+            } 
+            else if(angulo >= 90f) 
+            {
+                personagem.MostrarCima(PersonagemBase.Direcao.Esquerda);
+            }
+            else if(angulo >= 45f)
+            {
+                personagem.MostrarCima(PersonagemBase.Direcao.Direita);
+            }
+            else if(angulo >= -36f)
+            {
+                personagem.MostrarLado(PersonagemBase.Direcao.Direita);
+            }
+            else if(angulo >= -72f)
+            {
+                personagem.MostrarDiagonal(PersonagemBase.Direcao.Direita);
+            }
+            else if(angulo >= -108f)
+            {
+                personagem.MostrarFrente();
+            }
+            else if(angulo >= -144f)
+            {
+                personagem.MostrarDiagonal(PersonagemBase.Direcao.Esquerda);
+            }
+            else 
+            {
+                personagem.MostrarLado(PersonagemBase.Direcao.Esquerda);
+            }
         }
     }
 }
